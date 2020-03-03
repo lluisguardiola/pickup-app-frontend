2c1599c5ddfaa94a1ad8712ca35c49b8e65376c1
lluisguardiola <lluisguardiola.h@gmail.com>
2020-03-03 13:08:29 -0600

    Mapbox map rendering with event's address on showpage

--- src/components/events/EventShowPage.js
+++ src/components/events/EventShowPage.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import {Grid, Button} from "semantic-ui-react"
+import {Grid, Button, Icon} from "semantic-ui-react"
 import {withRouter, Link} from 'react-router-dom'
 import { connect } from 'react-redux'
+import ReactMapGL, { Marker } from 'react-map-gl'
+
+const MAPBOX_TOKEN = "pk.eyJ1IjoibGx1aSIsImEiOiJjazdjNDZ2Zm0wMGhxM2dtbnhla3MyY202In0.5OV7Y46VPigQ_LDRZyEo9Q"
 
 class EventShowPage extends React.Component {
 	state = {
-		event: null
+		event: null,
+		mapStyle: '',
+		viewport: {
+			width: "100vw",
+			height: "100vh",
+			latitude: 41.881832,
+			longitude: -87.623177,
+			zoom: 15
+		}
 	}
 	
 	componentDidMount = () => {
@@ -15,8 +26,40 @@ class EventShowPage extends React.Component {
 			this.setState({
 				event: event
 			})
+
+			this.renderMarkers()
 		})
-    }
+	}
+
+	onViewportChange = viewport => this.setState({viewport})
+
+	renderMarkers = () => {
+		fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${this.state.event.address}.json?country=US&types=address&access_token=${MAPBOX_TOKEN}`)
+			.then(resp => resp.json())
+			.then(mapboxData => {
+				console.log(mapboxData)
+				this.setState(prevState => {	
+					return {
+						...prevState,
+						viewport: {
+							...prevState.viewport,
+							longitude: mapboxData.features[0].geometry.coordinates[0],
+							latitude: mapboxData.features[0].geometry.coordinates[1]
+						}
+					
+				}})
+			})
+	}
+ 	
+	// handleAttendButton = () => {
+	// 	// event.preventDefault()
+
+	// 	fetch(`http://localhost:4000/`)
+	// 	.then(resp => resp.json())
+	// 	.then(data => {
+	// 		console.log(data)
+	// 	})
+	// }
 	
 	render () {
 		const {event} = this.state
@@ -45,6 +88,15 @@ class EventShowPage extends React.Component {
 									return <li key={person.id}>{person.name}</li>
 								})}
 							</ul>
+							{
+								this.props.user
+									?
+								<Button onClick={this.handleAttendButton}>Join Event</Button> //terniary inside button checking if user is in attendee list or not.
+									:
+								<Link to='/login'>
+									Log in to register to this event
+								</Link>
+							}
 						</Grid.Column>
 					</Grid.Row>
 					<Grid.Row>
@@ -65,6 +117,32 @@ class EventShowPage extends React.Component {
 								<Button>Delete</Button>
 							</div>
 						}
+					</Grid.Row>
+					<Grid.Row>
+							{
+								!event 
+									?
+								null
+									:
+								<ReactMapGL 
+									{...this.state.viewport}
+									mapboxApiAccessToken={MAPBOX_TOKEN} 
+									mapStyle="mapbox://styles/mapbox/streets-v11"
+									onViewportChange={this.onViewportChange}
+								>
+									<Marker 
+										latitude={this.state.viewport.latitude}
+										longitude={this.state.viewport.longitude}
+									>
+										<Icon 
+											color='red' 
+											name='map marker alternate'
+											size='big'
+										/>
+									</Marker>
+								</ReactMapGL>
+							}
+							
 					</Grid.Row>
 				</Grid>
 			)
